ff5c8883ac63c2b1ffc1fae51e3db39de9c1f4fb
Isaac <86988576+isaaclb98@users.noreply.github.com>
2025-02-07 14:42:04 -0500

    Refactor

---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StaticData", menuName = "Data/StaticData")]
+public class StaticData : ScriptableObject
+{
+    // Default player stats
+    [System.Serializable]
+    public class StaticPlayerStats
+    {
+        public int startingLevel = 1;
+        public int startingHealth = 10;
+        public int startingAttack = 1;
+        public int startingDefense = 1;
+        public int startingXp = 0;
+        public int startingXpToLevelUp = 83;
+    
+        // Player levelling constants
+        public double levelUpXpNeededMultiplier = 1.1;
+    }
+    
+    // Enemy stats
+    [System.Serializable]
+    public class EnemyStats
+    {
+        public string name;
+        public int health;
+        public int attack;
+        public int xpReward;
+        public int goldReward;
+    }
+
+    public EnemyStats[] enemies;
+    // Spider
+
+    // Creep
+
+    // Weapon stats
+}
