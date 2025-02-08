9e20c8d930572e742ab9b2eec282cdb795c33d6a
Isaac <86988576+isaaclb98@users.noreply.github.com>
2025-02-08 14:49:26 -0500

    Changes

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemData", menuName = "Data/ItemData")]
+public class ItemData : ScriptableObject
+{
+    public string itemName;
+    public int itemID;
+    public Sprite icon;
+    public string description;
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "Data/PlayerData")]
+public class PlayerData : ScriptableObject
+{
+    public int startingLevel = 1;
+    public int startingHealth = 10;
+    public int startingAttack = 1;
+    public int startingDefense = 1;
+    public int startingXp = 0;
+    public int startingXpToLevelUp = 83;
+    public double levelUpXpNeededMultiplier = 1.1;
+}
