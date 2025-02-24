b61f23ff3e0c53925c01303c3f4b358f8ea58fbd
NhanHo255 <jamesho2552002@gmail.com>
2025-02-23 19:57:39 -0500

    update gameSound

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-   
+    public void Start()
+    {
+        MusicManager.Instance.PlayMusic("MainMenuMusic");
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+        MusicManager.Instance.PlayMusic("AmbienceSound");
     }
 }
