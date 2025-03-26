3dcc3e151075fbb7854e51a0679e2aeb1186e588
Alberto-27 <apaul5@lakeheadu.ca>
2025-03-26 19:28:33 -0400

    PortaltoLevel2

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimplePortal : MonoBehaviour
+{
+    public string sceneToLoad = "Level2";
+
+    // Position to place the player at after teleporting
+    private Vector3 targetPosition = new Vector3(0.004f, -1.25f, 0.446f);
+    private Vector3 targetRotation = new Vector3(0f, -66.086f, 0f);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DontDestroyOnLoad(other.gameObject); // Keep player when loading new scene
+            SceneManager.sceneLoaded += (scene, mode) =>
+            {
+                other.transform.position = targetPosition;
+                other.transform.eulerAngles = targetRotation;
+            };
+
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
